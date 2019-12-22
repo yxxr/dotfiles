e9beb51fd3cc2044a9d9ba2c7a44fa8259416945
yxxr <yknr.public@gmail.com>
2019-12-22 14:47:38 +0900

    fix

--- install.sh
+++ install.sh
@@ -7,7 +7,11 @@ install_dein_vim() {
   curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_installer.sh
   sh ./dein_installer.sh ~/.cache/dein
 }
+install_tmux_conf() {
+  ln -s `pwd`/tmux/tmux.conf ~/.tmux.conf
+}
 #### main #####
 cd `dirname $0`
 install_vimrc
 install_dein_vim
+install_tmux_conf
